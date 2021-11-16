faf4dfe014d859d4a725c8df25720f32d31ef01d
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2021-11-16 01:17:02 +0100

    Update logic

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
 const {InfluxDB} = require('influx')
+const delay = require('delay')
+const got = require('got')
 const influx = new InfluxDB({
  host: 'influxdb',
- database: 'default'
+ database: 'mydb'
 })
-const delay = require('delay')
 const powers = [1, 2, 3, 4, 5, 6, 7, 8]
-const expectedTemperature = 19
+const expectedTemperature = 17
 const regulateInterval = 1000 * 5 * 60
 const awaitOnInterval = 1000 * 60
+const stoveUrl = 'http://ecoforest-api'
 
 class Room {
     async getCurrentTemperature() {
-        // influx.query(`
-        //     select * from response_times
-        //     where host = $<host>
-        //     order by time desc
-        //     limit 10
-        //   `)
-        const currentTemperature = 14 + Math.round(Math.random() * 10)
+        const result = await influx.query(`
+            SELECT last("temperature") FROM "home" WHERE time >= now() - 15m GROUP BY "sensor"
+        `)
 
-        console.log('[ROOM] Temperature is ' + currentTemperature)
-        return currentTemperature
+        const temperatures = result.map(r => r.last)
 
-        if (!temp) {
+        if (!temperatures.length) {
             throw new Error('Invalid temp')
         }
+
+        const currentTemperature = temperatures.reduce((t, v) => t+v, 0)/temperatures.length
+
+        return currentTemperature
     }
 }
 
 class Stove {
     constructor() {
-        this.power = 3
+        this.got = got.extend({
+            prefixUrl: stoveUrl
+        })
     }
     async start() {
         console.log('[STOVE] Start')
+        return this.got.put('status', {json: 1})
     }
     async stop() {
         console.log('[STOVE] Stop')
+        return this.got.put('status', {json: 0})
     }
     async setPower(power) {
-        this.power = power
         console.log('[STOVE] Set power ' + power)
+        return this.got.put('power', {json: power})
     }
     async getSummary() {
-        return {
-            humanStatus: 'running',
-            power: this.power,
-            mode: 'power'
-        }
+        return this.got.get('summary').json()
+    }
+    async setConvector(mode) {
+        return this.got.put('convector', {json: mode})
     }
 }
 
@@ -72,55 +76,77 @@ class Controller {
             await this.stove.start()
         }
 
-        console.log('[CONTROLLER] Putting stove with nominal power')
-        await this.stove.setPower(powers[Math.round(powers.length/2)])
-        console.log('[CONTROLLER] Awaiting stove to be started')
-        await this.waitForRunningStove()
-        console.log('[CONTROLLER] Starting temperature regulation')
-        await this.regulateTemperature()
+        try {
+            if (summary.humanStatus !== 'running') {
+                console.log('[CONTROLLER] Putting stove with nominal power')
+                await this.stove.setPower(powers[Math.round(powers.length/2)])
+                console.log('[CONTROLLER] Awaiting stove to be started')
+                await this.waitForRunningStove()
+            }
+            console.log('[CONTROLLER] Starting temperature regulation')
+            await this.regulateTemperature()
+        } catch (e) {
+            console.error(e)
+            await this.stove.stop()
+        }
     }
     async regulateTemperature() {
-        let cleverRegulating = false
+        let lastConvector
+
+        // Other idea : Begin with extremes, and decreases 2 by 2 or 1 by 1 (depends on temp diff ??)
+        // On depends on if temp increases or decreases !!
+        // ==> Example with expected 19, previous 19.4, current 19.35, it goes down but should NOT !
 
         while(true) {
             const currentPower = (await this.stove.getSummary()).power
             const currentPowerIndex = powers.indexOf(currentPower)
             const currentTemperature = await this.room.getCurrentTemperature()
 
-            if (expectedTemperature - currentTemperature > 2) {
-                console.log('[CONTROLLER] Very cold : putting stove max power')
-                cleverRegulating = false
-                await this.stove.setPower(powers[powers.length - 1])
-            } else if (currentTemperature - expectedTemperature > 2) {
-                console.log('[CONTROLLER] Very hot : putting stove min power')
-                cleverRegulating = false
-                await this.stove.setPower(powers[0])
-            } else {
-                if (!cleverRegulating) {
-                    console.log('[CONTROLLER] Correct temp ... beginning clever regulating with nominal power')
-                    await this.stove.setPower(powers[Math.round(powers.length/2)])
-                    cleverRegulating = true
-                } else {
-                    if (Math.abs(expectedTemperature - currentTemperature) < 0.3) {
-                        console.log('[CONTROLLER] Very good temp ... Do nothing')
-                    } else if (expectedTemperature > currentTemperature) {
-                        if (currentPowerIndex < powers.length) {
-                            const newPowerIndex = currentPowerIndex + Math.ceil((powers.length - 1 - currentPowerIndex)/2)
-                            console.log('[CONTROLLER] cleverRegulating - putting stove to ' + newPowerIndex)
-                            await this.stove.setPower(powers[newPowerIndex])
-                        } else {
-                            console.log('[CONTROLLER] cleverRegulating - Unable to put higher')
-                        }
-                    } else {
-                        if (currentPowerIndex > 0) {
-                            const newPowerIndex = currentPowerIndex - Math.floor(currentPowerIndex/2)
-                            console.log('[CONTROLLER] cleverRegulating - putting stove to ' + newPowerIndex)
-                            await this.stove.setPower(newPowerIndex)
-                        } else {
-                            console.log('[CONTROLLER] cleverRegulating - Unable to put lower')
-                        }
-                    }
+            if (currentTemperature > 22) {
+                // Galaxy Guardian
+                throw new Error('Too hot')
+            }
+
+            function higher() {
+                const nextStep = Math.ceil((powers.length - 1 - currentPowerIndex) / 2)
+                const nextIndex = currentPowerIndex + nextStep
+                return powers[nextIndex]
+            }
+
+            function lower() {
+                const previousIndex = Math.floor(currentPowerIndex / 2)
+                return powers[previousIndex]
+            }
+
+            const calculatedPower = (() => {
+                if (Math.abs(expectedTemperature - currentTemperature) < 0.25) {
+                    return currentPower
                 }
+
+                return expectedTemperature > currentTemperature
+                    ? higher()
+                    : lower()
+            })()
+
+            const convector = (() => {
+                if (powers.indexOf(calculatedPower) === 0) {
+                    return 'lowest'
+                }
+                if (powers.indexOf(calculatedPower) === powers.length - 1) {
+                    return 'highest'
+                }
+                return 'normal'
+            })()
+
+            console.log('[CONTROLLER] Regulating : ' + JSON.stringify({
+                currentTemperature, currentPower, expectedTemperature, calculatedPower, convector
+            }))
+
+            await this.stove.setPower(calculatedPower)
+
+            if (lastConvector !== convector) {
+                lastConvector = convector
+                this.stove.setConvector(convector)
             }
 
             await delay(regulateInterval)
